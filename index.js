85d5f89e265c6be6384f8562b24cbf24ed458133
Wesley Santos Martins <wesleysv61@gmail.com>
2023-12-31 10:40:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const keys = require("./keys");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+pgClient.on("connect", client => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch(err => console.log("PG ERROR", err));
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi att");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * FROM values");
+
+  res.send(values.rows);
+});
+
+app.post("/values", async (req, res) => {
+  if (!req.body.value) res.send({ working: false });
+
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [req.body.value]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log("Listening");
+});
---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pgUser: "wesley",
+  pgHost: "producao.postgres.database.azure.com",
+  pgDatabase: "producao",
+  pgPassword: "ProjetoInt123",
+  pgPort: "5432"
+};
