0a351ddce6bb6e5b6e25c1e7dac00bdf6ee05362
Wesley Santos Martins <wesleysv61@gmail.com>
2023-12-31 10:44:42 -0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   pgUser: "wesley",
-  pgHost: "producao.postgres.database.azure.com",
-  pgDatabase: "producao",
+  pgHost: "develop.postgres.database.azure.com",
+  pgDatabase: "develop",
   pgPassword: "ProjetoInt123",
   pgPort: "5432"
 };
